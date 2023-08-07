02ad7e80b40539c406ef175e4b69a95584bc5b24
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2023-08-06 23:55:51 -0300

    review, connection on remote MySql with JPA and Maven api

--- src/main/java/com/estudando/Main.java
+++ src/main/java/com/estudando/Main.java
@@ -2,14 +2,36 @@ package com.estudando;
 
 import com.estudando.dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(1, "Ana Paula Silva", "ana.silva@gmail.com");
-        Pessoa p2 = new Pessoa(2, "João Carlos Santos", "joao.santos@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Maria Eduarda Oliveira", "maria.oliveira@gmail.com");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("intro-Jpa");
+        EntityManager em = emf.createEntityManager();
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        Pessoa p = em.find(Pessoa.class, 1);
+        if (p == null) {
+            System.out.println("initialing database");
+
+            Pessoa p1 = new Pessoa(null, "Ana Paula Silva", "ana.silva@gmail.com");
+            Pessoa p2 = new Pessoa(null, "João Carlos Santos", "joao.santos@gmail.com");
+            Pessoa p3 = new Pessoa(null, "Maria Eduarda Oliveira", "maria.oliveira@gmail.com");
+
+            em.getTransaction().begin();
+            em.persist(p1);
+            em.persist(p2);
+            em.persist(p3);
+            em.getTransaction().commit();
+
+            System.out.println("initialed database");
+        } else {
+            System.out.println(p);
+        }
+
+        em.close();
+        emf.close();
+        System.out.println("Finalizado");
     }
 }
